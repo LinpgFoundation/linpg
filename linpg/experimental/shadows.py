be64118f6761d82d15165aa514f5ca5da80907f3
AnyLeftovers <37920527+yudonglin@users.noreply.github.com>
2023-12-18 20:14:02 -0800

    add new parameters to shadow rendering

--- linpg/experimental/shadows.py
+++ linpg/experimental/shadows.py
@@ -4,12 +4,11 @@ from ..interface import *
 # 阴影
 class Shadows:
     @staticmethod
-    def render(img: AbstractImageSurface, target_surface: ImageSurface) -> None:
+    def render(img: AbstractImageSurface, target_surface: ImageSurface, alpha: int = 100, percentage: int = 80) -> None:
         tans_rect: Rectangle = img.get_bounding_rect()
-        print(img.x + tans_rect.centerx, img.y + tans_rect.bottom, tans_rect.get_width() // 2, tans_rect.get_height() // 4)
         Draw.ellipse(
             target_surface,
-            (*Colors.GRAY[:3], 100),
+            (*Colors.GRAY[:3], alpha),
             (img.x + tans_rect.centerx, img.y + tans_rect.bottom),
-            (tans_rect.get_width() // 2, tans_rect.get_height() // 4),
+            (tans_rect.get_width() * percentage // 200, tans_rect.get_height() * percentage // 400),
         )
